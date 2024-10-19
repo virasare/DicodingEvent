fb9390566528365a9bb583af2c64f31de484a72c
virasare <virasare@gmail.com>
2024-10-20 04:02:18 +0800

    add rvFavorite dkk

--- app/src/main/java/com/dicoding/dicodingevent/ui/fragments/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/dicoding/dicodingevent/ui/fragments/favorite/FavoriteFragment.kt
@@ -1,31 +1,72 @@
 package com.dicoding.dicodingevent.ui.fragments.favorite
 
+import android.content.Intent
 import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.dicoding.dicodingevent.R
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dicoding.dicodingevent.databinding.FragmentFavoriteBinding
+import com.dicoding.dicodingevent.ui.DetailActivity
+import com.google.android.material.snackbar.Snackbar
 
 class FavoriteFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = FavoriteFragment()
-    }
+    private var _binding: FragmentFavoriteBinding? = null
+    private val binding get() = _binding!!
 
-    private val viewModel: FavoriteViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
+    private lateinit var adapter: FavoriteAdapter
+    private val favoriteViewModel: FavoriteViewModel by viewModels()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.rvFavorite.layoutManager = LinearLayoutManager(context)
+        adapter = FavoriteAdapter { event ->
+            val intent = Intent(context, DetailActivity::class.java).apply {
+                putExtra("event_id", event.id)
+            }
+            startActivity(intent)
+        }
+        binding.rvFavorite.layoutManager = GridLayoutManager(requireContext(), 2)
+        binding.rvFavorite.adapter = adapter
+
+        favoriteViewModel.events.observe(viewLifecycleOwner) { events ->
+            adapter.submitList(events)
+        }
+
+        favoriteViewModel.isLoading.observe(viewLifecycleOwner) { isLoading ->
+            showLoading(isLoading)
+        }
+
+        favoriteViewModel.snackbarText.observe(viewLifecycleOwner) { message ->
+            message?.let {
+                Snackbar.make(binding.root, it, Snackbar.LENGTH_SHORT).show()
+            }
+        }
+
+        favoriteViewModel.fetchEvents()
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
 }
--- app/src/main/java/com/dicoding/dicodingevent/ui/fragments/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/dicoding/dicodingevent/ui/fragments/favorite/FavoriteViewModel.kt
@@ -1,7 +1,43 @@
 package com.dicoding.dicodingevent.ui.fragments.favorite
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.dicoding.dicodingevent.data.response.EventResponse
+import com.dicoding.dicodingevent.data.response.ListEventsItem
+import com.dicoding.dicodingevent.data.retrofit.ApiConfig
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class FavoriteViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _events = MutableLiveData<List<ListEventsItem>>()
+    val events: LiveData<List<ListEventsItem>> = _events
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _snackbarText = MutableLiveData<String>()
+    val snackbarText: LiveData<String> = _snackbarText
+
+    fun fetchEvents() {
+        _isLoading.value = true
+        val client = ApiConfig.getApiService().getEvents(1)
+        client.enqueue(object : Callback<EventResponse> {
+            override fun onResponse(call: Call<EventResponse>, response: Response<EventResponse>) {
+                _isLoading.value = false
+                if (response.isSuccessful) {
+                    _events.value = response.body()?.listEvents
+                }else{
+                    _snackbarText.value = "Gagal memuat data: ${response.message()}"
+                }
+            }
+
+            override fun onFailure(call: Call<EventResponse>, t: Throwable) {
+                _isLoading.value = false
+                _snackbarText.value = "Koneksi gagal: ${t.message}"
+            }
+        })
+    }
 }
